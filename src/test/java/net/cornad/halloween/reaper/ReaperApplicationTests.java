f6fa32a53e3684a7f468e92b669926b67779e25d
belloacpace <39655681+belloacpace@users.noreply.github.com>
2020-10-28 22:24:33 -0400

    Spring Initializer template start.

---
+++ src/test/java/net/cornad/halloween/reaper/ReaperApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cornad.halloween.reaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
